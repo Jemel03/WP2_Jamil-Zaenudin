e226479af443272cdbd81f25e3b71e25052494b7
Jemel03 <152607626+Jemel03@users.noreply.github.com>
2024-03-21 18:31:48 +0700

    Add files via upload

--- contoh1.php
+++ contoh1.php
@@ -1,9 +1,9 @@
 <?php
-class contoh1 extends CI_Controller
+class Contoh1 extends CI_Controller
 {
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo"Nama saya Jamil Zaenudin, udah itu aja.";
+        echo"Nama Saya Jamil Zaenudin,Udah Itu aja";
     }
 }
